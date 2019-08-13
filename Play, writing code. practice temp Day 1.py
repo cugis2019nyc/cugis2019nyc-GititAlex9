79d46dffdb3772dfa3260e72f2cd338428757792
GititAlex9 <54040057+GititAlex9@users.noreply.github.com>
2019-08-13 13:43:29 -0400

    Add files via upload

---
+++ Play, writing code. practice temp Day 1.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 11:25:42 2019
+
+@author: columbia
+"""
+
+print("hello world, 10 divided by 5 equals") #welcome, information on divison
+print(10/5)
+print("Answer")
+print("I am learning to code, 100 divided by 3 is")
+print(100/3)
+
+print("Grand Answer isss.............. 5 times 4 is")
+print(5*4)
+
+print("5+4=")
+print(5+4)
+
+print("5-1=")
+print(5-1)
+
+print(5**2) #squaring
+print((5*6)/3)
+
+def add(a,b):
+    add = a+b
+    print(add)
+add(5555.5,105555)
+
+def adds(a,b,c):
+    adds = a+b+c
+    print("The sum of",a,",",b,"and",c,"='s",adds)
+    
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+multiply(5,5)
+
+def bh(a,b):
+    bh = a*b*1/2
+    print("The area of this triangle is",bh)
+bh(5,8)
+
+def trapezoid(a,b,h):
+    trapezoid = ((a+b)/2)*h
+    print("Base, base, height")
+    print("Trapezoid area is",trapezoid)
+trapezoid(4,8,2)
+
+def circle(a):
+    circle = ((3.1415926*a)**2)
+    print("Pi, radi, squared, plug in radius")
+    print("Circle area is",circle)
+circle(2)
+
+
+
+
+
+
+
+
+
+
+
+
+#greet a person by  their name
+print("Hello Alex")
+a ="Alex"
+print("Hello",a)    
+
+name = input 
+
+#def NameOfFunction(parameters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if (5+4) is (9) 
+#print("correct")
+#path = '9'
+#if path:('9')
+#    print "True"
+#else:
+ #   print "False"
+    
